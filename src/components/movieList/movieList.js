851bb13c9b7f506238420cbc329dabd9f26516b7
Navneet <pranavnavneet11@gmail.com>
2024-04-30 14:59:18 +0530

    updated the code again

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -8,19 +8,16 @@ const MovieList = () => {
     const [movieList, setMovieList] = useState([])
     const {type} = useParams()
 
-    useEffect(() => {
-        getData()
-    }, [])
-
-    useEffect(() => {
-        getData()
-    }, [type])
-
     const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
         .then(data => setMovieList(data.results))
     }
+
+    useEffect(() => {
+        getData()
+    })
+
 
     return (
         <div className="movie__list">
